f625a657304f01b6553ea8d0e442e3c4792651c2
crossagent <150273112@qq.com>
2023-03-06 22:59:27 +0800

    commit jibenmoxing

--- yiming/views.py
+++ yiming/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.http import HttpResponse
+
+
+# newapp/views.py
+
+from django.http import HttpResponse
+
+
+def call_chatGpt(request):
+    if request.method == 'POST':
+        # 处理POST请求
+        return HttpResponse('POST请求成功')
+    else:
+        # 处理其他类型的请求
+        return HttpResponse('不支持的请求类型')
